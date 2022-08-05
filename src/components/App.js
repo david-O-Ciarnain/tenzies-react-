1c48b0f5593e119bbd854c0cd6285cb3fe5b1e9f
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-08-05 17:55:23 +0200

    Map and displayed all  numbers in randomNumberArray

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import Dice from "./Dice"
 
 export default function App(){
 
+    const [diceNumbers,setDiceNumbers] = useState(allNewDice)
+
+    
     function allNewDice(){
         const randomNumbersArray = []
-
-        for(let i = 0; i < 10; i++){
+        
+        for(let i = 0; i < 15; i++){
             let radnomNumber = Math.floor(Math.random() * 6) + 1
             randomNumbersArray.push(radnomNumber)
         }
-        console.log(randomNumbersArray)
+        return randomNumbersArray
     }
-    allNewDice()
+    const dice = diceNumbers.map(dice => <Dice value={dice}/> )
 
     return(
         <main className="main-body">
             <div className="dice-container">
-                <Dice value={2} />
-                <Dice value={2} />
-                <Dice value={2} />
-                <Dice value={2} />
-                <Dice value={2} />
-                <Dice value={2} />
-                <Dice value={2} />
-                <Dice value={2} />
-                <Dice value={2} />
-                <Dice value={2} />
+           {dice}
             </div>
         </main>
     )
