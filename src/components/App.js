fcc3f1990e396f234ebd45add2a5fad867202fbb
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-08-05 17:07:34 +0200

    added dice component and styled it

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from "react"
-
+import Dice from "./Dice"
 
 export default function App(){
 
     return(
-        <main className="main-body"></main>
+        <main className="main-body">
+            <div className="dice-container">
+                <Dice value={2} />
+                <Dice value={2} />
+                <Dice value={2} />
+                <Dice value={2} />
+                <Dice value={2} />
+                <Dice value={2} />
+                <Dice value={2} />
+                <Dice value={2} />
+                <Dice value={2} />
+                <Dice value={2} />
+            </div>
+        </main>
     )
 }
---
+++ src/components/Dice.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Dice(props){
+
+    return(
+        <p className="dice">{props.value}</p>
+    )
+}
